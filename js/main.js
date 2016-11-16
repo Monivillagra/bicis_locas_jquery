548b56f032fb84cea8151853f8faf6fac57b2b84
Monivillagra <villagra.ramirez@gmail.com>
2016-11-16 20:29:07 -0300

    span al fin visibles

--- js/main.js
+++ js/main.js
@@ -1,119 +1,29 @@
-// traemos los elementos
-// var name = document.getElementById('name');
-// var lastname = document.getElementById('lastname');
-// var email = document.getElementById('input-email');
-// var password = document.getElementById('input-password');
-// var select = document.getElementsByTagName('select')[0];
-
-// var div_checkbox = document.getElementsByClassName('checkbox')[0];
-// var checkbox = div_checkbox.getElementsByTagName('input')[0];
-// var span_checkbox = div_checkbox.getElementsByTagName('span')[0];
-
-
-
-// viculo los eventos
-// name.onblur = validarNombre;
-// lastname.onblur = validarApellido;
-// email.onblur = validarEmail;
-// password.onblur = validarPassword;
-// select.onblur = validarSelect;
-// checkbox.onclick = tachar;
-
-// funciones
-// function tachar() {
-// 	span_checkbox.classList.toggle('tachado');
-// }
-
-// function escribir(str) {
-// 	document.getElementById('form-signup-heading').innerHTML = str;
-// }
-
-// function limpiarSpans(clase) {
-// 	var div = document.getElementsByClassName(clase)[0];
-// 	var hijos_spans = div.getElementsByTagName('span');
-// 	for (var i=0 ; i<hijos_spans.length ; ++i) {
-// 		div.removeChild(hijos_spans[i]);
-// 	}
-// }
-
-// function validarNombre() {
-// 	limpiarSpans('name-container');
-// 	var inpName = document.getElementById("name").value;  //valor de nombre
-	// if(inpName == null || inpName.length == 0 || !(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/).test(inpName)){ //valida que no tenga espacio vacíos, ni que se llene con espacios en blanco, que la primera letra sea mayúscula y no contenga números
-// 		var divName = document.getElementsByClassName("name-container")[0];
-// 		var spName = document.createElement("span");
-// 		var msgName = document.createTextNode("El nombre que ingresaste no es correcto");
-// 		spName.appendChild(msgName);
-// 		divName.appendChild(spName);
-// 		return false;
-// 	}
-// }
-
-// function validarApellido() {
-// 	limpiarSpans('lastname-container');
-// 	var inpLastName = document.getElementById("lastname").value; //valor de apellido
-	// if(inpLastName == null || inpLastName.length == 0 || !(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/).test(inpLastName)){ //valida que no tenga espacio vacíos, ni que se llene con espacios en blanco, que la primera letra sea mayúscula y no contenga números
-// 		var divLastName = document.getElementsByClassName("lastname-container")[0];
-// 		var spLastName = document.createElement("span");
-// 		var msgLastName = document.createTextNode("El apellido que ingresaste no es correcto");
-// 		spLastName.appendChild(msgLastName);
-// 		divLastName.appendChild(spLastName);
-// 		return false;
-// 	}
-// }
-// function validarEmail() {
-// 	limpiarSpans('email-container');
-// 	var inpEmail = document.getElementById("input-email").value;  //valor de email
-	// if(!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(inpEmail))){ //valida que contenga letras o números, mayúsculas o minúsculas y que la última parte es  
-// 		var divEmail = document.getElementsByClassName("email-container")[0];
-// 		var spEmail = document.createElement("span");
-// 		var msgEmail = document.createTextNode("El correo electrónico que ingresaste no es correcto");
-// 		spEmail.appendChild(msgEmail);
-// 		divEmail.appendChild(spEmail);
-// 		return false;
-// 	}
-// }
-// function validarPassword() {
-// 	var inpPassword = document.getElementById("input-password").value; //valor de contraseña
-	// limpiar los spans
-	// var div = document.getElementsByClassName('form-group')[0];
-	// var spans = div.getElementsByTagName('span');
-	// for(var i=0 ; i < spans.length ; i++){
-	// 	div.removeChild(spans[i]);
-	// }
-
-	// if (inpPassword.length < 6 || inpPassword == "password" || inpPassword == "123456" || inpPassword == "098754") { // valida que el password tenga al menos 6 dígitos y que tenga al menos una letra mayúscula y una minúscula y no sea alguna de las claves que el ejercicio ponía como prohibidas
-// 		var divPassword = document.getElementsByClassName("form-group")[0];
-// 		var spPassword = document.createElement("span");
-// 		var msgPassword = document.createTextNode("Debe contener al menos 6 caracteres");
-// 		spPassword.appendChild(msgPassword);
-// 		divPassword.appendChild(spPassword);
-// 		return false;
-// 	}
-// }
-// function validarSelect() {
-// 	var inpOpciones = document.getElementsByTagName("select")[0].selectedIndex; //valor del select
-	// limpiar los spans
-	// var div = document.getElementsByClassName('form-group')[1];
-	// var spans = div.getElementsByTagName('span');
-	// for(var i=0 ; i < spans.length ; i++){
-	// 	div.removeChild(spans[i]);
-	// }
-
-	// if(inpOpciones == null || inpOpciones == 0) { //valida que el select tenga al menos una opción seleccionada
-// 		var divSelect = document.getElementsByClassName("form-group")[1];
-// 		var spSelect = document.createElement("span");
-// 		var msgSelect = document.createTextNode("No has seleccionado ninguna opción");
-// 		spSelect.appendChild(msgSelect);
-// 		divSelect.appendChild(spSelect);
-// 		return false;
-// 	}
-// }
-
-// function validateForm(){
-// 	validarNombre();
-// 	validarApellido();
-// 	validarEmail();
-// 	validarPassword();
-// 	validarSelect();
-// }
+function validateForm() {
+	// nombre
+	var name = $('#name').val();// var name = document.getElementById("name").value;  //valor de nombre
+	if(name == null || name.length === 0 || !(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/).test(name)){ //valida que no tenga espacio vacíos, que la primera letra sea mayúscula y que solo tenga caracteres.
+		$('#name').parent().append('<span>¡Escriba el primer caracter de su nombre en Mayúscula.!</span>');
+	}
+	// apellido
+	var lastName = $('#lastname').val();// var lastName = document.getElementById("lastname").value; //valor de apellido
+	if(lastName == null || lastName.length === 0 || !(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/).test(lastName)){ //valida que no tenga espacio vacíos, que la primera letra sea mayúscula y que solo tenga caracteres.
+		$('#lastname').parent().append('<span>¡Escriba el primer caracter de su apellido en Mayúscula.!</span>');
+	}
+	// correo
+	var email = $('#input-email').val();// var email = document.getElementById("input-email").value;  //valor de email
+	if(!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email))){ //valida que tenga letras o números, mayúsculas o minúsculas y que tenga un formato valido de dominio. 
+		$('#input-email').parent().append('<span>¡Dirección de Correo Electrónico No Válido.!</span>');
+	}
+	// contraseña
+	var password = $('#input-password').val();// var password = document.getElementById("input-password").value; //valor de contraseña
+	if (password == null || password.length < 6) { // valida que la contraseña no tenga espacios y que tenga un minimo de 6 dígitos.
+		$('#input-password').parent().append('<span>¡Contraseña No Valida, debe tener como mínimo 6 Caracteres!</span>');
+	}else if ( password === '123456' || password === '098754' || password === 'password' ) { //valida que la contraseña no sea igual a "password", "123456" o "098754".
+        	$('#input-password').parent().append('<span>¡Su Contraseña No es Segura! No puede ser igual a "123456", "password" o "098754"!</span>');   
+       }
+  	// selector
+	var bicis = $('select').val();//valor del select
+	if(bicis == null || bicis === '0') { //valida que el select tenga una opción seleccionada
+		$('select').parent().append('<span>¡Seleccione su Tipo de Bici!</span>');
+	}
+}
